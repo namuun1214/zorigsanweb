f727209147c38dc4650293ff3ef3a154370b850a
Enkhmunkh <enkhmunkh@Enkhmunkhs-MacBook-Pro.local>
2022-03-24 17:49:20 +0800

    initialization

---
+++ src/components/Input/Input.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  InputStyle,
+  InputS,
+  InputIcon,
+  Label,
+  InputCon,
+  InputHintOptional,
+  InputOptionalCon,
+  InputLabel,
+  InputSendButton,
+} from '.';
+import {  SearchIcon , BackIcon, OptionalIcon} from '../../assets/icons'
+
+export const Input = ({ type, label, hint, placeholder, optional, border }) => {
+  return (
+    <InputCon>
+      {label && hint !== '' && (
+        <Label>
+          <InputLabel>{label}</InputLabel>
+          <InputHintOptional>{hint}</InputHintOptional>
+        </Label>
+      )}
+      <InputStyle border={border}>
+        {type === 'search' ? (
+          <>
+            <SearchIcon/>
+            <InputS search placeholder={placeholder} />
+          </>
+        ) : type === 'send' ? (
+          <>
+            <InputS arrow placeholder={placeholder} />
+            <InputSendButton>
+              <BackIcon/>
+            </InputSendButton>
+          </>
+        ) : (
+          <InputS placeholder={placeholder} />
+        )}
+      </InputStyle>
+      {optional && (
+        <InputOptionalCon>
+          <OptionalIcon/>
+          <InputHintOptional>{optional}</InputHintOptional>
+        </InputOptionalCon>
+      )}
+    </InputCon>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// import { graphql, useStaticQuery } from "gatsby";
+import * as React from "react";
+const IndexPage = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default IndexPage;
